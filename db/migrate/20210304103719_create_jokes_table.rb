6cb32fc0b90606913bbf9696e92747f002606347
nick <nicholas_scalione@yahoo.com>
2021-03-04 05:56:11 -0500

    created jokes table

--- db/migrate/20210304103719_create_jokes_table.rb
+++ db/migrate/20210304103719_create_jokes_table.rb
@@ -1,4 +1,10 @@
 class CreateJokesTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :jokes do |t|
+      t.string :title
+      t.string :type
+      t.text :punchline
+      t.string :characters
+      t.string :setting
   end
 end
